d9b8e9f67cc1fc3302d3cf9cf76ac3819cf1556d
lizuxian <tanzheng.li@outlook.com>
2025-03-19 00:27:36 +0800

    feature: 新增数据字典导入导出

--- banking-core/src/main/java/com/ling/banking/core/mapper/DictMapper.java
+++ banking-core/src/main/java/com/ling/banking/core/mapper/DictMapper.java
@@ -1,7 +1,10 @@
 package com.ling.banking.core.mapper;
 
+import com.ling.banking.core.pojo.dto.ExcelDictDTO;
 import com.ling.banking.core.pojo.entity.Dict;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface DictMapper extends BaseMapper<Dict> {
 
+
+    void insertBatch(List<ExcelDictDTO> list);
 }
--- banking-core/src/main/java/com/ling/banking/core/service/DictService.java
+++ banking-core/src/main/java/com/ling/banking/core/service/DictService.java
@@ -2,6 +2,10 @@ package com.ling.banking.core.service;
 
 import com.ling.banking.core.pojo.entity.Dict;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +17,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface DictService extends IService<Dict> {
 
+    @Transactional
+    void importData(InputStream inputStream);
+
+    List<Dict> listByParentId(Long parentId);
+
+    List listDictData();
 }
